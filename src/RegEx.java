51d888f43b9ebb3cd9396f8b10e9c4a19d696828
DOvaKriiD <ssvinogradoff@gmail.com>
2022-10-07 20:38:53 +0300

    first view on Hub

--- src/RegEx.java
+++ src/RegEx.java
@@ -2,6 +2,9 @@ import java.util.regex.Pattern;
 
 public class RegEx {
     public static boolean isCorrectIP(String checkingString) {
-        return true;
+        Pattern pattern = Pattern.compile(
+                "((2[1-4]\\d|25[1-5]|\\d|\\d\\d|1\\d\\d)\\.){3}" +
+                        "(\\d|\\d\\d|1\\d\\d|21-4\\d|251-5)");
+        return pattern.matcher(checkingString).matches();
     }
 }
--- src/RegExTest.java
+++ src/RegExTest.java
@@ -6,5 +6,21 @@ public class RegExTest {
     public void correctIP()
     {
         Assert.assertEquals(true, RegEx.isCorrectIP("0.0.0.0"));
+        Assert.assertEquals(true, RegEx.isCorrectIP("234.24.0.0"));
+        Assert.assertEquals(true, RegEx.isCorrectIP("255.255.255.0"));
+        Assert.assertEquals(true, RegEx.isCorrectIP("127.0.0.1"));
+        Assert.assertEquals(true, RegEx.isCorrectIP("123.0.0.0"));
+        Assert.assertEquals(true, RegEx.isCorrectIP("1.255.123.4"));
+    }
+
+    @Test
+    public void incorrectIP()
+    {
+        Assert.assertEquals(false, RegEx.isCorrectIP("abc.def.gha.bcd"));
+        Assert.assertEquals(false, RegEx.isCorrectIP("1300.6.7.8"));
+        Assert.assertEquals(false, RegEx.isCorrectIP("256.0.0.0"));
+        Assert.assertEquals(false, RegEx.isCorrectIP(".0.0.0"));
+        Assert.assertEquals(false, RegEx.isCorrectIP("34.z.25.123"));
+        Assert.assertEquals(false, RegEx.isCorrectIP("1.1.1.."));
     }
 }
